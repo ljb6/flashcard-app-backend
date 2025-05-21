ad594c3ba1cd379f202483c1143a106fb5477a7c
Lucca <luccajbecker@gmail.com>
2025-05-20 22:47:23 -0300

    feat: add flashcards routing setup

--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,22 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	//"github.com/ljb6/flashcard-app-backend/internal/flashcards"
 )
 
 func InitializeRoutes(router *gin.Engine, db *sql.DB) {
+
+	// flashcards
+	// flashcardsRepo := flashcards.NewFlashcardRepository(db)
+	// flashcardsService := flashcards.NewFlashcardService(flashcardsRepo)
+	// flashcardsHandler := flashcards.NewFlashcardHandlers(flashcardsService)
+
+	//flashcardGroup := router.Group("/flashcards")
+
 	router.GET("/ping", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "pong",
     })
   })
+
 }
