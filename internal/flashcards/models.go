b1feea61948af2a1764e36a40aca403e6daf865a
Lucca <luccajbecker@gmail.com>
2025-05-20 22:34:05 -0300

    feat: add flashcard model

---
+++ internal/flashcards/models.go
@@ -0,0 +1,10 @@
+package flashcards
+
+import "time"
+
+type Flashcard struct {
+	ID        int       `jsond:"id"`
+	Front     string    `jsond:"front"`
+	Back      string    `jsond:"back"`
+	CreatedAt time.Time `json:"created_at"`
+}
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/ljb6/flashcard-app-backend/database"
+import (
+	"github.com/ljb6/flashcard-app-backend/database"
+	router "github.com/ljb6/flashcard-app-backend/routes"
+)
 
 func main() {
 	db := database.ConnectDB()
 	defer db.Close()
+
+	router.InitializeServer(db)
 }
